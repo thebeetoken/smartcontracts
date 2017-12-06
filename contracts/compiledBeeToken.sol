0b9bd51316faaae491eb5a725df0532587a89ca8
rob <rob@thebeetoken.com>
2017-12-06 14:12:19 -0800

    Add Pausable

--- contracts/compiledBeeToken.sol
+++ contracts/compiledBeeToken.sol
@@ -316,6 +316,47 @@ contract BeeToken is StandardToken, BurnableToken, Ownable {
 }
 
 
+contract Pausable is Ownable {
+    event Pause();
+    event Unpause();
+
+    bool public paused = false;
+
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is not paused.
+   */
+    modifier whenNotPaused() {
+        require(!paused);
+        _;
+    }
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is paused.
+   */
+    modifier whenPaused() {
+        require(paused);
+        _;
+    }
+
+  /**
+   * @dev called by the owner to pause, triggers stopped state
+   */
+    function pause() onlyOwner whenNotPaused public {
+        paused = true;
+        Pause();
+    }
+
+  /**
+   * @dev called by the owner to unpause, returns to normal state
+   */
+    function unpause() onlyOwner whenPaused public {
+        paused = false;
+        Unpause();
+    }
+}
+
+
 contract BeeTokenOffering is Pausable {
 
 
