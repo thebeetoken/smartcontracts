c3b06a01576c341fdb61c07f9915807da209f91e
boyxgc <boyxgc@gmail.com>
2018-01-11 22:51:54 -0800

    Remove hardcoded gas

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,24 +3,24 @@ var BeeTokenOffering = artifacts.require("./BeeTokenOffering.sol");
 var BeePayments = artifacts.require("./BeePayments.sol");
 
 module.exports = function(deployer, network, accounts) {
-    console.log("Accounts: " + accounts);
+    console.log(`Accounts: ${accounts}`);
 
     var beeToken = null;
     var beeOffering = null;
     var beePayments = null;
 
-    return deployer.deploy(BeeToken, {from: accounts[1], gas: 4700000}).then(() => {
+    return deployer.deploy(BeeToken, {from: accounts[1]}).then(() => {
         return BeeToken.deployed().then(instance => {
             beeToken = instance;
         });
     }).then(() => {
-        return deployer.deploy(BeeTokenOffering, 2000, accounts[1], 20000, beeToken.address, {from: accounts[1], gas: 4700000}).then(() => {
+        return deployer.deploy(BeeTokenOffering, 2000, accounts[1], 20000, beeToken.address, {from: accounts[1]}).then(() => {
             return BeeTokenOffering.deployed().then(instance => {
                 beeOffering = instance;
             });
         })
     }).then(() => {
-        return deployer.deploy(BeePayments, accounts[0], {from: accounts[0], gas: 4700000}).then(() => {
+        return deployer.deploy(BeePayments, accounts[0], {from: accounts[0]}).then(() => {
             return BeePayments.deployed().then(instance => {
                 beePayments = instance;
             })
