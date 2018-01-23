ce63b5b095a75e79b1a7cb961e1556d8640d5931
boyxgc <boyxgc@gmail.com>
2018-01-22 20:31:17 -0800

    Remove commented code

--- test/BeeTokenOffering.test.js
+++ test/BeeTokenOffering.test.js
@@ -34,16 +34,7 @@ contract('BeeTokenOffering constructor', function (accounts) {
     async function balanceOf(user) {
         return (await token.balanceOf(user)).toNumber();
     }
-    /*
-      it("should not allow to contribute more than allowed by the cap", async function() {
-          await token.setTokenOffering(offering.address, 0);
-          await offering.startOffering(300);
-          await whitelistTierA(user3);
-          if ((await offering.currentTime()) <= (await offering.doubleTime())) {
-            await util.assertRevert(sendTransaction(16, user3));
-          }
-      });
-    */
+    
     it("should sell tokens at a prespecified rate", async function () {
         await token.setTokenOffering(offering.address, 0);
         await offering.startOffering(300);
@@ -72,52 +63,18 @@ contract('BeeTokenOffering constructor', function (accounts) {
         await token.setTokenOffering(offering.address, 0);
         await util.assertRevert(sendTransaction(0, user5));
     });
-    /*
-      it("should reach the cap", async function() {
-          //await token.setTokenOffering(offering.address, 0);
-          //await offering.startOffering(300);
-          await offering.whitelistTierA([user5], {from:owner});
-          await sendTransaction(13, user5);
-          assert.equal(await offering.fundingCapReached(), true);
-      });
-    */
+
     it("should not allow non-owners to call ownerSafeWithdraw", async function () {
         await token.setTokenOffering(offering.address, 0);
         await util.assertRevert(offering.allocateTokensBeforeOffering(user3, util.oneEther, util.twoEther, { from: user2 }));
     });
-    /*    
-      it("should allow transfers to registered users, even beyond caps", async function(){
-        await whitelistTierA(user2);
-        await whitelistTierA(user3);
-    
-        await offering.allocateTokens(user2, util.oneEther, util.oneEther, {from:owner});
-    
-        await offering.allocateTokens(user3, util.twoEther, util.oneEther, {from:owner});
-        let addrList = [user2, user3];
-        let amtsList = [util.oneEther, util.twoEther];
-        let user2_balance = await(offering.contributions(user2));
-        let user2_token_balance = await(token.balanceOf(user2));
-        await offering.allocateTokens(user2, util.oneEther, util.oneEther, {from:owner});
-        await offering.allocateTokens(user3, util.twoEther, util.twoEther, {from:owner});
-    
-    
-        let reached_cap = await offering.fundingCapReached();
-        let user2_balance_after = await(offering.contributions(user2));
-        let user2_token_balance_after = await(token.balanceOf(user2));
-        console.log(user2_balance.add(util.oneEther) + " " + user2_balance_after + " " + reached_cap);
-        assert.equal(user2_balance.add(util.oneEther).toNumber(), user2_balance_after.toNumber(), "user2 ether balance should have increased by 1");
-        assert.equal(user2_token_balance.add(util.oneEther).toNumber(), user2_token_balance_after.toNumber(), "user2 token balance should have increased by 1");
-    
-      });
-    */
 
-    it("should allow transfers to unregistered users", async function () {
+    it("should allow transfers to pre-sale users", async function () {
         await token.setTokenOffering(offering.address, 0);
         await offering.allocateTokensBeforeOffering(user4, util.oneEther, util.oneEther, { from: owner });
     });
 
 });
-
 
 contract('Whitelist Crowdsale', function (accounts) {
     var owner = accounts[0];
